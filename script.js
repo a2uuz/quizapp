ca959a3c7788cd9fa2f756f5a6efcb2b976994f0
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-02-03 10:37:19 +0300

    done

--- script.js
+++ script.js
@@ -1,46 +1,24 @@
-// const quiz = [
-// ['do you like me?', 'for real..'],
-// ['would you die for me?', 'if you could!'],
-// ['i do really wanna die', 'in peace']
-// ];
-
-
-// function app(game){
-//     let ask = "";
-//     for (let i = 0; i < game.length; i++) {
-//         ask += prompt(`${game}`);
-//     }
-//     return ask;
-// }
-
-// document.querySelector('main')
-// .innerHTML = `<ol>${app(quiz)}<ol>`
-
-const quiz = [
-['do you javascript?', 'for real..'],
-['would you die for react?', 'if you could!'],
-['do you really wanna learn Node..', 'in peace?']
+const quiz = [ 
+  ['How many planets are in the Solar System?', '8'],
+  ['How many continents are there?', '7'],
+  ['How many legs does an insect have?', '6'],
+  ['What year was JavaScript created?', '1995']  
 ];
 
-
+let score = 0;
 // --------------------------------------------------------
 
+for (let i = 0; i < quiz.length; i++) {
+    let question = quiz[i][0];
+    let answer = quiz[i][1];
+    let response = prompt(question);
 
-function app(game){
-    
-    for (let i = 0; i < game.length; i++) {
-        let ask = prompt(`${quiz[i][0]}, ${quiz[i][1]}`);
-        let score = 0;
-        if (ask.toLowerCase() === "yes") {
-        score += 1;
-        }else{
-        score = `None :(`;
-        }
-
+    if (response === answer) {
+        score++;
     }
-    return score;
 }
+
 // --------------------------------------------------------
 
-document.querySelector('main')
-.innerHTML = `You got ${app(quiz)} out of 3 questions correct`;
+let html = `<h1>You got ${score} out of 4 questions correct</h1>`;
+document.querySelector('main').innerHTML = html; 
